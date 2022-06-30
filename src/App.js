ba067eb02bc549bf8d1cd8f0a6d24c44f5675ba1
ashenafiyessuf <ashenafi.yessuf@allmyhomes.com>
2022-06-30 15:37:54 +0200

    json database created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import './App.css';
+import DataList from './Database/Database.json';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [data, setData] = useState(0);
+console.log(DataList)
   return (
-    <div className="App">
-       <h2>Hi there!</h2>
+    <div>
+       hi
     </div>
   );
 }
